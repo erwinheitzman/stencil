27e14926a34378307fd9a683413abd31c2e3ed54
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-06-16 13:00:55 -0400

    chore(jsdoc): add jsdoc to taskTest function

--- src/cli/task-test.ts
+++ src/cli/task-test.ts
@@ -1,7 +1,11 @@
 import type { Config, TestingRunOptions } from '../declarations';
 import { IS_NODE_ENV } from '../compiler/sys/environment';
 
-export const taskTest = async (config: Config) => {
+/**
+ * Initiates running tests against a Stencil project
+ * @param config the Stencil configuration to use while running tests
+ */
+export const taskTest = async (config: Config) : Promise<void> => {
   if (!IS_NODE_ENV) {
     config.logger.error(`"test" command is currently only implemented for a NodeJS environment`);
     return config.sys.exit(1);
